5a9d1a89e37f3fd92fd2064b7f2a870c169d7df9
Soraya Tapia <[soraya.a.t@gmail.com]>
2024-09-25 20:00:14 -0300

    trabajo4,creacion de 3 nuevas funciones (agregar,mostrar,eliminar)

--- app/src/main/java/com/example/ktrabajo1/Trabajo3.kt
+++ app/src/main/java/com/example/ktrabajo1/Trabajo3.kt
@@ -20,14 +20,57 @@ class Usuario(
     }
 }
 
+class Listado {
+    private val listaUsuarios = mutableListOf<Usuario>()
 
-fun main() {
+    //Función AGREGAR
+    fun agregarUsuario(usuario: Usuario) {
+        listaUsuarios.add(usuario)
+        println("Usuario ${usuario.nombre} agregado a la lista.")
+    }
 
-    val usuario1 = Usuario(nombre = "Nimona", edad = 25, trabajo = "cartera")
-    val usuario2 = Usuario(nombre = "Arale", edad = 37, trabajo = null, referencia = usuario1)
+    // Función ELIMINAR
+    fun eliminarUsuario(nombre: String) {
+        val usuarioAEliminar = listaUsuarios.find { it.nombre == nombre }
+        if (usuarioAEliminar != null) {
+            listaUsuarios.remove(usuarioAEliminar)
+            println("Usuario $nombre eliminado de la lista.")
+        } else {
+            println("Usuario $nombre no encontrado.")
+        }
+    }
 
-    usuario1.mostrarDatos()
-    usuario2.mostrarDatos()
+    // Función MOSTRAR
+    fun mostrarLista() {
+        if (listaUsuarios.isEmpty()) {
+            println("La lista de usuarios está vacía.")
+        } else {
+            println("Lista de usuarios:")
+            listaUsuarios.forEach { it.mostrarDatos() }
+        }
+    }
 }
 
+fun main() {
+    val listado = Listado()
 
+    // Crear algunos usuarios
+    val usuario1 = Usuario(nombre = "Nimona", edad = 25, trabajo = "cartera")
+    val usuario2 = Usuario(nombre = "Arale", edad = 37, trabajo = null, referencia = usuario1)
+    val usuario3 = Usuario(nombre = "Akane", edad = 18, trabajo = "científica")
+    val usuario4 = Usuario(nombre = "Goku", edad = 42, trabajo = "luchador", referencia =null)
+    val usuario5 = Usuario(nombre = "Kang", edad = 19, trabajo = "Actor")
+
+
+    listado.agregarUsuario(usuario1)
+    listado.agregarUsuario(usuario2)
+    listado.agregarUsuario(usuario3)
+    listado.agregarUsuario(usuario4)
+    listado.agregarUsuario(usuario5)
+
+
+    listado.mostrarLista()
+
+    listado.eliminarUsuario("Akane")
+    listado.mostrarLista()
+}
